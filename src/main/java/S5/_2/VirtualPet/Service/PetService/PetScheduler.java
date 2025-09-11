309b55ca79875a33db6a3448e0f38e38de69696d
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-09-11 16:23:10 +0200

    propperties form pet implented in application connfig

--- src/main/java/S5/_2/VirtualPet/Service/PetService/PetScheduler.java
+++ src/main/java/S5/_2/VirtualPet/Service/PetService/PetScheduler.java
@@ -2,10 +2,12 @@ package S5._2.VirtualPet.Service.PetService;
 
 import S5._2.VirtualPet.Model.Pet;
 import S5._2.VirtualPet.Model.enums.Status;
+import S5._2.VirtualPet.Config.PetProperties;
 import S5._2.VirtualPet.Repositories.PetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -16,6 +18,7 @@ import java.util.List;
 public class PetScheduler {
 
     private final PetRepository petRepository;
+    private final PetProperties properties;
 
     @Scheduled(fixedRate = 3600000)
     public void increaseNeedsOverTime() {
@@ -26,7 +29,8 @@ public class PetScheduler {
             if (pet.getLasFedAt() != null) {
                 long hoursSinceFed = ChronoUnit.HOURS.between(pet.getLasFedAt(), now);
                 if (hoursSinceFed > 0) {
-                    pet.setHunger(Math.min(100, pet.getHunger() + (int) hoursSinceFed * 5));
+                    pet.setHunger(Math.min(100, pet.getHunger() +
+                            (int) hoursSinceFed * properties.getHunger().getIncreasePerHour()));
                 }
             }
             if (pet.getLastInteractedAt() != null) {
--- src/main/java/S5/_2/VirtualPet/VirtualPetApplication.java
+++ src/main/java/S5/_2/VirtualPet/VirtualPetApplication.java
@@ -2,8 +2,10 @@ package S5._2.VirtualPet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VirtualPetApplication {
 
 	public static void main(String[] args) {
